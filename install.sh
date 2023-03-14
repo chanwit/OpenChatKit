da94063337869a2e731232649a7e3fb17206fdeb
Chanwit Kaewkasi <chanwit@gmail.com>
2023-03-14 19:02:46 +0700

    add install.sh script
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip3 install datasets==2.10.1
+pip3 install loguru==0.6.0
+pip3 install netifaces==0.11.0
+pip3 install transformers==4.21.1
+pip3 install wandb==0.13.10
+pip3 install zstandard==0.20.0
